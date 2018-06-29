eb64eb730d0dc5dddba824a45ee1ca6153dc5f5e
sephib <js.berry@gmail.com>
2018-06-29 11:32:05 +0300

    add function to create metadata for video

--- main.py
+++ main.py
@@ -36,6 +36,24 @@ def convert_audio_file(audio_file: Path, audio_format: str = 'mp3'):
         logger.debug(f'convert {audio_file} to {new_file}')
 
 
+def create_youtube_metadata(video_tuple):
+    meta_dict={}
+    meta_dict['category'] = 'Music'
+    #TODO get ALYA Name
+    meta_dict['title'] = f' נוסח אשכנז -  {video_tuple[0].parts[-1]} - פרשת {video_tuple[0].parts[-2]}  '
+    meta_dict['description'] = f' {video_tuple[0].parts[-1]} קריאה בתורה לפרשת  '
+    meta_dict['description-file'] = None
+    meta_dict['tags'] = ' קריאה בתורה', f'{video_tuple[0].parts[-2]}'
+    meta_dict['privacy'] = 'public'
+    meta_dict['publish-at'] = None
+    meta_dict['recording-date'] = '2017'
+    meta_dict['default-language'] = 'he'
+    meta_dict['thumbnail'] = video_tuple[0]
+    meta_dict['playlist'] = f''
+
+    return meta_dict
+
+
 def main():
     # clean bad files
     # TODO Remove files starting with '.' or '_'
@@ -51,22 +69,30 @@ def main():
 
     # Get list of relevent files
     l = []
-    for i in Path(audio_dir):
+    for i in Path(audio_dir).rglob('*.jpg'):
         l.append(i)
         if len(l) > 1:
             if l[-2].parent != l[-1].parent:
                 j = l.pop()
                 print(f'process: {len(l)}')
+                ffjpg = sorted(l)
+                ffjpg[-1], ffjpg[-2] = ffjpg[-2], ffjpg[-1]  # switch mavtir and haftara
+                ffmp3 = sorted([f for f in l[0].parent.rglob('*.mp3')])
+                # create pairs
+                if len(ffjpg) != len(ffmp3):
+                    print(f'PROBLEM: number of files not equal in {l[0].parent}')
+                    continue
+                video_pair = [i for i in zip(ffjpg, ffmp3)]
+                for v in video_pair:
+                    create_youtube_metadata(v)
+
                 l = []
                 l.append(j)
             else:
                 pass
-    ffjpg = sorted([f for f in p.iterdir() if (f.suffix in ['.jpg'])])
-    ffjpg[-1], ffjpg[-2] = ffjpg[-2], ffjpg[-1]  # switch mavtir and haftara
-    ffmp3 = sorted([f for f in p.iterdir() if (f.suffix in ['.mp3'])])
 
-    # create pairs
-    video_pair = [i for i in zip(ffjpg, ffmp3)]
+
+
 
     # for video in glob.glob(extension_list):
     #     mp3_filename = os.path.splitext(os.path.basename(video))[0] + '.mp3'
